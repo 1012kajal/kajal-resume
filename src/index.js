1a32af80da4311876f2d1a2c9f73237b929f5ea5
1012kajal <1012kajalpatil@gmail.com>
2025-07-16 02:11:39 -0400

    Create index.js
    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./style.css"; // Optional, we’ll add style.css next
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
